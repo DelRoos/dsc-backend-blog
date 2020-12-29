d4f82277ea926586150390c2cabbc073d8a990eb
pantaLuc <lucapameni@gmail.com>
2020-12-29 12:38:56 +0100

    DEBUG=False collection des fichiers statiques

--- DSC_Blog/settings.py
+++ DSC_Blog/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'sc+@-30a2-fwywx&$r-@imyrrcvrr3mf3w!f+lwi^fu0bb2u-b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["backend-dsc.herokuapp.com","127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
+#"backend-dsc.herokuapp.com","127.0.0.1"
 
 
 # Application definition
