273e62ff2ee1a6b0b1d417137eedf3a0db48ee44
pantaLuc <lucapameni@gmail.com>
2020-12-31 22:04:19 +0100

    pagination

--- utils/pagination.py
+++ utils/pagination.py
@@ -9,15 +9,15 @@ class PostLimitOffsetPagination(LimitOffsetPagination):
     max_limit = 10
 
 class PostPageNumberPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 6
 
 
 class FormationLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 2
+    default_limit = 6
     max_limit = 10
 
 class FormationPageNumberPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 6
 
 class EventPageNumberPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 4
